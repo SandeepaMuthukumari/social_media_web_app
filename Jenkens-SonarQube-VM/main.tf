f2e20658d1ec32ff63175b9d739e251220ab06f3
SandeepaMuthukumari <166691172+SandeepaMuthukumari@users.noreply.github.com>
2025-04-05 10:41:58 +0530

    Update main.tf

--- Jenkens-SonarQube-VM/main.tf
+++ Jenkens-SonarQube-VM/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
-  ami                    = "ami-080e1f13689e07408"      
+  ami                    = "ami-0287a05f0ef0e9d9a"      #change ami id for different region
   instance_type          = "t2.large"
-  key_name               = "dockerize"            
+  key_name               = "Linux-VM-Key7"              #change key name as per your setup
   vpc_security_group_ids = [aws_security_group.Jenkins-VM-SG.id]
   user_data              = templatefile("./install.sh", {})
 
